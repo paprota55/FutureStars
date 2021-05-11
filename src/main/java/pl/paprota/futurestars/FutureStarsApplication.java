d4501898555ed69262d8aa78229589ce7c183aa8
paprota55 <paprota55@gmail.com>
2021-05-11 14:12:15 +0200

    Initial project

---
+++ src/main/java/pl/paprota/futurestars/FutureStarsApplication.java
@@ -0,0 +1,13 @@
+package pl.paprota.futurestars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureStarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FutureStarsApplication.class, args);
+    }
+
+}
